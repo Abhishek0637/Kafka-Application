8ec2a87b7aa57fa33613989ba7a0104c0abecbfb
Abhishek0637 <abhisheksaini16092001@gmail.com>
2024-02-11 18:04:51 +0530

    Initial commit

---
+++ locationUpdate/locationUpdate/src/main/java/com/Kafka/locationUpdate/LocationUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.Kafka.locationUpdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationUpdateApplication.class, args);
+	}
+
+}
---
+++ locationUpdate/locationUpdate/src/test/java/com/Kafka/locationUpdate/LocationUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kafka.locationUpdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
